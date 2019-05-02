cdd0c50a06ba59eb0da394ca61ce1c50614e9620
gavroman <gavroman1998@gmail.com>
2019-05-02 15:37:16 +0300

    Added gitignore + test battle map

---
+++ game_field/hex_tiles/dirt.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="dirt01_c" tilewidth="48" tileheight="48" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="48" height="48" source="../hex_textures/dirt01_c.png"/>
+ </tile>
+</tileset>
