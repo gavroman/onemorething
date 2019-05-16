5a4078b918ccfb7f571e8dce9323c09cea960646
gavroman <gavroman1998@gmail.com>
2019-05-17 00:29:47 +0300

    done

--- project/Game/Game.cpp
+++ project/Game/Game.cpp
@@ -14,8 +14,8 @@ Game::Game(const int &map_id) {
 }
 
 void Game::run_game(const std::string xml_file_path) {
-    //sf::RenderWindow window(sf::VideoMode(1920, 1080), "One More Thing", sf::Style::Fullscreen);
-    sf::RenderWindow window(sf::VideoMode(1920, 750), "One More Thing");
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "One More Thing", sf::Style::Fullscreen);
+    //sf::RenderWindow window(sf::VideoMode(1920, 750), "One More Thing");
 
     Map battle_field("../source/game_map/" + xml_file_path);
     battle_field.draw_map(window);
@@ -25,13 +25,10 @@ void Game::run_game(const std::string xml_file_path) {
     
     sf::Vector2i pos_pressed(0, 0);
     sf::Vector2i pos_released(0, 0);
-    //sf::Vector2i pos_moved(0, 0);
 
     int distance = 5;           // радиус хода (надо брать из персонажа)
     bool active = false;        // выбрана ли клетка ()
 
-    battle_field.draw_map(window);
-    window.display();
 
     std::shared_ptr<Character> test_char = std::make_shared<Scout>(45);
     battle_field.update_cell(test_char, 45);
@@ -46,6 +43,22 @@ void Game::run_game(const std::string xml_file_path) {
                     window.close();
                     break;
 
+                case sf::Event::MouseMoved: {
+                    sf::Vector2i new_pos = sf::Mouse::getPosition(window);
+                    sf::Vector2f pos_moved(new_pos.x, new_pos.y);
+                    int hvd_cell_id = battle_field.get_cell_id_from_pos(pos_moved);
+                    if (!battle_field.is_empty(hvd_cell_id) and !active) {
+                        move_area = battle_field.find_move_area(hvd_cell_id, matrix, distance);
+                        sf::Color color(20, 30, 82, 100);
+                        battle_field.drop_highlight_cells();
+                        battle_field.add_highlight_cells(move_area[distance], color, color);
+                        //std::cout << hvd_cell_id << std::endl;
+                    } else if (!active){                    
+                        battle_field.drop_highlight_cells();
+                    }
+                    break;
+                }    
+
                 case sf::Event::MouseButtonPressed:
                     pos_pressed = sf::Mouse::getPosition(window);
                     break;
@@ -55,6 +68,10 @@ void Game::run_game(const std::string xml_file_path) {
                     if (pos_pressed == pos_released) {
                         sf::Vector2f pos(pos_pressed.x, pos_pressed.y);
                         int cell_id = battle_field.get_cell_id_from_pos(pos);
+                        if (active and !battle_field.is_in_area(move_area, cell_id)) { //если был клик не в зоне, снятие подсветки, деактивация персонажа
+                            battle_field.drop_highlight_cells();
+                            active = false;
+                        }
                         if (battle_field.is_passable(cell_id)) {
                         //эта клетка проходима 
                             sf::Color color_trace(20, 240, 45, 100);
@@ -63,8 +80,6 @@ void Game::run_game(const std::string xml_file_path) {
                                 // Значит надо рисовать путь
                                 //battle_field.set_active_char(cell_id, false);
                                 std::vector<int> route = battle_field.find_route(cell_id, move_area, matrix);
-
-
                                 active = false;
                                 battle_field.drop_highlight_cells();
                                 battle_field.add_highlight_cells(route, color_trace, color_trace);
@@ -80,29 +95,9 @@ void Game::run_game(const std::string xml_file_path) {
                                 battle_field.add_highlight_cells(move_area[distance], color, color);
                                 battle_field.add_highlight_cells({move_area[0][0]}, color_trace, color_trace);
                             }
-                        } else { //если была нажата кнопка не в зоне, снятие подсветки 
-                                battle_field.drop_highlight_cells();
-                                active = false;
                         }
                     }
                     break;
-
-                case sf::Event::MouseMoved: {
-                    sf::Vector2i new_pos = sf::Mouse::getPosition(window);
-                    sf::Vector2f pos_moved(new_pos.x, new_pos.y);
-                    int hvd_cell_id = battle_field.get_cell_id_from_pos(pos_moved);
-                    if (!battle_field.is_empty(hvd_cell_id) and !active) {
-
-                        move_area = battle_field.find_move_area(hvd_cell_id, matrix, distance);
-                        sf::Color color(20, 30, 82, 100);
-                        battle_field.drop_highlight_cells();
-                        battle_field.add_highlight_cells(move_area[distance], color, color);
-                        std::cout << hvd_cell_id << std::endl;
-                    } else if (!active){                    
-                        battle_field.drop_highlight_cells();
-                    }
-                    break;
-                }
     
                 default: 
                     break;
